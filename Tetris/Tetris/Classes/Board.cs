1b11b3fa36dc84d5a1b38279d3d6ac326ffa043c
Слапік Євген Святославович <y.slapik@officekiev.fozzy.lan>
2016-08-26 15:14:08 +0300

    First tetris commit

---
+++ Tetris/Tetris/Classes/Board.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Tetris.Classes
+{
+    class Board
+    {
+        private int _columnNumber;
+        private int _rowNumber;
+        private int _score;
+        private int _filledLines;
+        private Tetromino _currentFigure;
+        private Label[,] _blockControls;
+        public Board(Grid tetrisGrid)
+        {
+            _rowNumber = tetrisGrid.RowDefinitions.Count;
+            _columnNumber = tetrisGrid.ColumnDefinitions.Count;
+            _score = 0;
+            _filledLines = 0;
+            _blockControls = new Label[_rowNumber,_columnNumber];
+            for (int i = 0; i < _columnNumber; i++)
+            {
+                for (int j = 0; j < _rowNumber; j++)
+                {
+                    _blockControls[i,j] = new Label();
+                    _blockControls[i,j].Background = Brushes.Transparent;
+                    _blockControls[i,j].BorderBrush = Brushes.Gray;
+                    _blockControls[i,j].BorderThickness = new Thickness(1,1,1,1);
+                    Grid.SetRow(_blockControls[i,j],j);
+                    Grid.SetColumn(_blockControls[i,j], i);
+                    tetrisGrid.Children.Add(_blockControls[i,j]);
+                }
+            }
+            _currentFigure = new Tetromino();
+            DrawFigure();
+        }
+        public int GetScore()
+        {
+            return _score;
+        }
+
+        public int GetFilledLines()
+        {
+            return _filledLines;
+        }
+
+        private void DrawFigure()
+        {
+            var position = _currentFigure.GetFigurePosition();
+            var shape = _currentFigure.GetFigureShape();
+            var shapeColor = _currentFigure.GetFigureColor();
+            foreach (var block in shape)
+            {
+
+            }
+        }
+
+        private void EraseFigure()
+        {
+
+        }
+
+    }
+}
---
+++ Tetris/Tetris/Classes/Tetromino.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Tetris.Classes
+{
+    public class Tetromino
+    {
+
+        private Brush[] _availableColors =
+        {
+            Brushes.DarkGoldenrod,
+            Brushes.Crimson,
+            Brushes.LightSalmon,
+            Brushes.DarkBlue,
+            Brushes.ForestGreen,
+            Brushes.LightGreen,
+            Brushes.OrangeRed,
+            Brushes.Teal,
+            Brushes.DarkMagenta,
+            Brushes.Gold
+        };
+        private Point _currentPos; //позиция фигуры
+        private Point[] _shape;    //форма фигуры
+        private Brush _color;       //цвет фигуры
+        private bool _rotatable;   //можно ли вращать        
+        
+        public Point GetFigurePosition()
+        {
+            return _currentPos;
+        }
+        public Brush GetFigureColor()
+        {
+            return _color;
+        }
+        public Point[] GetFigureShape()
+        {
+            return _shape;
+        }
+
+        public void MoveLeft()
+        {
+            _currentPos.X -= 1;
+        }
+        public void MoveRight()
+        {
+            _currentPos.X += 1;
+        }
+        public void MoveDown()
+        {
+            _currentPos.Y += 1;
+        }
+        public void Rotate()
+        {
+            if (_rotatable)
+            {
+                for (int i = 0; i < _shape.Length; i++)
+                {
+                    double x = _shape[i].X;
+                    _shape[i].X = _shape[i].Y*-1;
+                    _shape[i].Y = x;
+                }
+            }
+        }
+                               
+
+        public Tetromino()
+        {
+            _currentPos = new Point(0,0);
+            _color = Brushes.Transparent;
+            _shape = generateShape();
+        }
+
+        private Point[] generateShape()
+        {
+            var randomShape = new Random().Next(0, 7);
+            switch (randomShape)
+            {
+                case 0:    //I
+                    _rotatable = true;
+                    _color = _availableColors[new Random().Next(0, _availableColors.Length)];
+                    return new Point[]
+                    {
+                      new Point(0,0), 
+                      new Point(-1,0), 
+                      new Point(1,0), 
+                      new Point(2,0)
+                    };   
+                    
+                case 1:    //J
+                    _rotatable = true;
+                    _color = _availableColors[new Random().Next(0, _availableColors.Length)];
+                    return new Point[]
+                    {
+                      new Point(1,-1), 
+                      new Point(-1,0), 
+                      new Point(0,0), 
+                      new Point(1,0)
+                    };
+                case 2: // L
+                    _rotatable = true;
+                    _color = _availableColors[new Random().Next(0, _availableColors.Length)];
+                    return new Point[]
+                    {
+                      new Point(0,0), 
+                      new Point(-1,0), 
+                      new Point(1,0), 
+                      new Point(-1,1)
+                    };
+                case 3:  //o
+                    _rotatable = true;
+                    _color = _availableColors[new Random().Next(0, _availableColors.Length)];
+                    return new Point[]
+                    {
+                      new Point(0,0), 
+                      new Point(0,1), 
+                      new Point(1,0), 
+                      new Point(1,1)
+                    };
+                case 4:  //S
+                    _rotatable = true;
+                    _color = _availableColors[new Random().Next(0, _availableColors.Length)];
+                    return new Point[]
+                    {
+                      new Point(0,0), 
+                      new Point(-1,0), 
+                      new Point(0,1), 
+                      new Point(1,0)
+                    };
+                case 5: //T
+                    _rotatable = true;
+                    _color = _availableColors[new Random().Next(0, _availableColors.Length)];
+                    return new Point[]
+                    {
+                      new Point(0,0), 
+                      new Point(-1,0), 
+                      new Point(0,-1), 
+                      new Point(1,1)
+                    };
+                case 6://Z
+                    _rotatable = true;
+                    _color = _availableColors[new Random().Next(0, _availableColors.Length)];
+                    return new Point[]
+                    {
+                      new Point(0,0), 
+                      new Point(-1,0), 
+                      new Point(0,1), 
+                      new Point(1,1)
+                    };
+            }
+        }
+
+    }
+}
